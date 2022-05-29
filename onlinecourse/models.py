748280124523971ee4416ed973593dce8e325fbb
akram <gretiudem@gmail.com>
2022-05-29 13:53:48 +0000

    add Question, Choice models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -105,10 +105,12 @@ class Question(models.Model):
     # Foreign key to lesson
     # question text
     # question grade/mark
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    question _text = models.CharField(max_length=1000)
-    question_grade = models.CharField(max_length=200)
-
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=1000, default='Question default')
+    question_grade = models.IntegerField(default=12)
+    
+    def __str__(self):
+        return "Question: " + self.question_text
 
     # <HINT> A sample model method to calculate if learner get the score of the question
     def is_get_score(self, selected_ids):
@@ -126,11 +128,10 @@ class Question(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
- class Choice(models.Model):
-     choice_text = models.CharField(max_length=1000)
-     is_correct = models.BooleanField(default=true)
+class Choice(models.Model):
      question = models.ForeignKey(Question, on_delete=models.CASCADE)
-     submissions = models.ManyToManyField(submissions)
+     choice_text = models.CharField(max_length=1000)
+     is_correct = models.BooleanField(default=False)
 
 
 # <HINT> The submission model
